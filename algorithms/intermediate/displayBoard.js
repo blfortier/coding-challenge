697c64ca0ef4f3771fe1d2d35461bc8dbb46c02a
Brittney <brittfort34@gmail.com>
2019-02-07 18:56:53 +0000

    Created a function called isGameOver to process the end of the game

--- algorithms/intermediate/displayBoard.js
+++ algorithms/intermediate/displayBoard.js
@@ -1,7 +1,7 @@
 
 let vertical = "\t" + "---" + "+" + "---"  + "-" + "+" + "---";
 
-const position = ["", "", "", "", "", "", "", "", ""];
+let position = ["", "", "", "", "", "", "", "", ""];
 
 
 //let moveCount = (player_1_moves.length + player_2_moves.length);
@@ -53,23 +53,41 @@ function didWin() {
         player_that_won = (position[2] == "X") ? console.log("Player 1 won!") : console.log("Player 2 won!");
         return true;
     } else {
-        console.log("Nobody won...");
+        //console.log("Nobody won...");
         return false;
     }
 }
+
+function isDraw() {
+    
+    if ((player_1_moves.length + player_2_moves.length) > 8) {
+        console.log("Looks like the board is full, nobody won...");
+        console.log("Game Over!");
+        resetBoard();
+        return true;   
+    } else {
+        console.log(player_1_moves.length + player_2_moves.length);
+        return false;
+    }
+}
+
+
+function isGameOver() {
+    
+    if (isDraw || didWin) 
+        resetBoard();
+}
+
 
 // This function checks whether a players move
 // is valid. Accepts an int, where the player
 // would like to put their token
 function isValidMove(move) {
-        
-    // Store true or false
-    let result;
     
     // If the passed in number is beween 0 and 8 (the tic tac toe board),
     // and the spot is empty, return true. If the spot is taken, return false
     if (move >= 0 && move <= 8) {
-        if (!position[move]) {
+        if (position[move] == "") {
             console.log("That spots open");
             return true;
         } else {
@@ -78,28 +96,17 @@ function isValidMove(move) {
         }
     } else {
         console.log("Please enter a number between 0 and 8");
-        result = false;
-    }
-  
-    // Return the result, true or false
-    return result;
-}
-
-
-function isDraw() {
-    
-    if ((player_1_moves.length + player_2_moves.length) > 9) {
-        console.log("Game is over, nobody won...");
-        return true;   
-    } else {
-        console.log(player_1_moves.length + player_2_moves.length);
         return false;
     }
 }
 
+
 function applyMove(move, player) {
 
-    position[move] = fillPlayerArrays(move, player);
+    let token = (player == 1) ? "X" : "O";
+    
+    position[move] = token;
+    //boardPlaces[move] = fillPlayerArrays(move, player);
 }
 
 
@@ -111,23 +118,17 @@ function processMove(move, player) {
     } else {
         applyMove(move, player);
 
+        if (player == 1)
+            player_1_moves.push(move);
+        else
+            player_2_moves.push(move);
+            
+    
         if (isDraw() && didWin()){
-            console.log("TODO");
+            isGameOver();
             return true;
         }
     }
-}
-
-function fillPlayerArrays(move, player) {
-    
-    let token = (player == 1) ? "X" : "O";
-        
-    if (token == "X") 
-        player_1_moves.push(move);
-    else 
-        player_2_moves.push(move);
-        
-    return token;
 }
 
 
@@ -144,9 +145,9 @@ function importBoard(string) {
        
        // Keep track of the player's move
        // Store them in an array
-       if (string[i] == "X" || string[i] == "x") {
+       if (string[i] == "X") {
             player_1_moves.push(i);
-       } else if (string[i] == "O" || string[i] == "o") {
+       } else if (string[i] == "O") {
            player_2_moves.push(i);
        }
     }
@@ -163,8 +164,8 @@ function displayBoard() {
     
     // Display the tic tac toe board
     let preRow1 = "\t   |    |  ";
-    let row_1 = "\t" + position[0] + "   | " + position[1] + "   | " + position[2];
-    let row_2 = "\t" +position[3] + "  | " + position[4] + "   | " + position[5];
+    let row_1 = "\t" + position[0] + "  | " + position[1] + "  | " + position[2];
+    let row_2 = "\t" +position[3] + "  | " + position[4] + "  | " + position[5];
     let row_3 = "\t" +position[6] + "  | " + position[7] + "  | " + position[8];
 
     for (let i = 1; i < 2; i++) {
@@ -186,35 +187,31 @@ function displayBoard() {
     
 }
 
-
-    let boardPlaces = ["", "", "X", "X", "", "", "O", "", "O"];
-    importBoard(boardPlaces);
     
-   
-    processMove(7, 1);
-   
-   
-   
-    //let move = isValidMove(2);
-
-   // displayBoard();
+   // let boardPlaces = ["", "", "X", "X", "", "", "O", "X", "O"];
+   //importBoard(boardPlaces);
     
-   // didWin(boardPlaces);
-
-   // isGameOver(); 
-  
-  // didWin(position);
+     processMove(5, 1);
+     processMove(3, 1);
+     processMove(1, 2);
+     processMove(0, 1);
+     processMove(2, 1);
+     processMove(8, 2);
+     processMove(7, 1);
+     processMove(4, 2);
+     processMove(6,2);
+     
+ 
+    console.log();
+    
+    displayBoard();
    
-   
-   console.log("pos: ", position);
-   console.log("player 1: ", player_1_moves);
-   console.log("player 2: ", player_2_moves);
-   console.log("Did somebody win? ", didWin());
-   console.log("Is there a draw? ", isDraw());
-   console.log();
-   
-   displayBoard();
-
+    console.log("pos: ", position);
+    console.log("player 1: ", player_1_moves);
+    console.log("player 2: ", player_2_moves);
+    
+    console.log("Did somebody win? ", didWin());
+    console.log("Is there a draw? ", isDraw());
 
 
 
